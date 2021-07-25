60c87fe142ccfc52367a85644ecffdb24d6ea4a4
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 17:11:24 -0400

    Update WebsiteGenerator.java

--- src/main/java/oop/assignment3/ex43/base/WebsiteGenerator.java
+++ src/main/java/oop/assignment3/ex43/base/WebsiteGenerator.java
@@ -6,24 +6,24 @@ import java.io.FileWriter;
 
 public class WebsiteGenerator {
 
+    // Declare string for siteName, author, and path
     String siteName;
     String author;
     String path;
 
     public String createWebsite() {
-
-
-        String directory = path + siteName;
+        // create string directory and set it equal to path + site name
+        String directory = path + "\\" + siteName;
         File newFolder = new File(directory);
 
+        // make new directory called new folder
         newFolder.mkdir();
         return directory;
-
-
     }
 
     public String createJSFolder() {
 
+        // create string directory and set it equal to path + site name for JS
         String directory = path + siteName + "/js";
         File newFolder = new File(directory);
 
@@ -35,6 +35,7 @@ public class WebsiteGenerator {
 
     public String createCSSFolder() {
 
+        // create string directory and set it equal to path + site name for CSS
         String directory = path + siteName + "/css";
         File newFolder = new File(directory);
 
@@ -44,22 +45,20 @@ public class WebsiteGenerator {
     }
 
     public String createHTMl() {
-
-        String directory = path + siteName + "/index.html";
+        // create string directory and set it equal to path + site name for HTML
+        String directory = path + "\\" + siteName + "\\index.html";
 
         String htmlContent = "<title> " + siteName + " </title>\n<meta> " + author + " </meta>";
 
         File file = new File(directory);
-
         try {
-
+            // write content for the html file
             BufferedWriter writer = new BufferedWriter(new FileWriter(file));
             writer.write(htmlContent);
             writer.close();
             return directory;
 
         } catch(Exception ex) {
-
             return "END";
         }
 
