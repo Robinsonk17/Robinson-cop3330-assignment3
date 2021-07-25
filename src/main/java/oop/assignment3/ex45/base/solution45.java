1b55c57bba0048e195c63196df552008631227db
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 17:11:39 -0400

    Update solution45.java

--- src/main/java/oop/assignment3/ex45/base/solution45.java
+++ src/main/java/oop/assignment3/ex45/base/solution45.java
@@ -6,146 +6,35 @@ package oop.assignment3.ex45.base;
 
 // import classes
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class solution45 {
 
     public static void main(String[] args) throws IOException {
-// prompt scanner for user input
+        String output, str;
+
+        // Initialize scanner for user input.
         Scanner input = new Scanner(System.in);
 
-        System.out.print("\nEnter the output file : ");
+        // Prompt user for the name of the output file.
+        System.out.print("Enter the output file : ");
+        output = input.next();
 
-        String outputfile = input.next();
+        str = "";
 
-        ReadFile rf = new ReadFile();
+        // Re-initialize scanner to read from the file.
+        input = new Scanner(new File("exercise45_input.txt"));
 
-        rf.readile();
-
-        ArrayList<String> list = rf.getdata();
-
-        WriteFile wr = new WriteFile(outputfile, list);
-
-        wr.writefile();
-
-        System.out.println("\nThe Number of Modifications are : "+wr.getModification());
-
-
-    }
-
-}
-
-
-class ReadFile{
-
-    private ArrayList<String> list;
-    private Scanner sc;
-
-
-    public ReadFile() throws FileNotFoundException {
-
-        list = new ArrayList<String>();
-
-        File file = new File("C:\\Users\\Ankit\\Desktop\\exercise45_input.txt");
-        sc = new Scanner(file);
-
-    }
-
-    public void readile() {
-
-        try
-        {
-            int i=0;
-
-            while( sc.hasNext() )
-            {
-
-                String filedata = sc.nextLine();
-
-                list.add(filedata);
-
-
-            }
-        }
-        finally
-        {
-            sc.close();
+        // For each line read, replace the word "utilize" with the word "use".
+        while (input.hasNext()) {
+           str += input.nextLine().replaceAll("utilize", "use") + "\n";
         }
 
-    }
-
-
-    public ArrayList<String> getdata() {
-
-        return list;
-
-    }
-
-}
-
-class WriteFile{
-
-
-    private FileWriter writer;
-    private int count_Modification;
-    private ArrayList<String> list;
-
-    public WriteFile(String name, ArrayList<String> list) throws IOException {
-
-
-        writer = new FileWriter("C:\\Users\\Ankit\\Desktop\\"+name);
-        this.list = list;
-
-
-    }
-
-
-    public void writefile() throws IOException {
-
-
-        for(int i=0;i<list.size();i++) {
-
-            String temp = list.get(i);
-
-            String str = "utilize";
-
-            int index = 0;
-
-            while (true)
-            {
-                index = temp.indexOf(str, index);
-                if (index != -1)
-                {
-                    count_Modification ++;
-                    index += str.length();
-                }
-                else {
-                    break;
-                }
-            }
-
-            temp = temp.replaceAll("utilize", "use");
-
-
-            writer.write(temp+"\n");
-
-
-        }
-
+        // Write the string to a file.
+        BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+        writer.write(str);
         writer.close();
-
     }
-
-    public int getModification() {
-
-        return count_Modification;
-
-    }
-
-
 }
