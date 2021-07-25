6949e17059ac6abd5965650247e555574677803d
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 17:10:58 -0400

    Update solution42.java

--- src/main/java/oop/assignment3/ex42/base/solution42.java
+++ src/main/java/oop/assignment3/ex42/base/solution42.java
@@ -1,20 +1,19 @@
 package oop.assignment3.ex42.base;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /*
  *  UCF COP3330 Summer 2021 Assignment 3 Solution
  *  Copyright 2021 Keondez Robinson
  */
-public class solution42
-{
+public class solution42 {
+        public static void main(String[] args) throws FileNotFoundException {
+            // Parse input from the file.
+            String[] parsedData = parseInput("exercise42_input.txt");
 
-        public static void main(String[] args) {
-
-
-            String inputNotParsed;
-            inputNotParsed = readInto("src/main/java/ex42/exercise42_input.txt");
-
-            String[] parsedData;
-            parsedData = parseInput(inputNotParsed);
-
+            // Format data as specified in the problem.
             System.out.println("Last      First     Salary");
             System.out.println("--------------------------");
             System.out.println(parsedData[0] + "      " + parsedData[1] + "       " + parsedData[2]);
@@ -26,32 +25,18 @@ public class solution42
             System.out.println(parsedData[18] + "  " + parsedData[19] + "   " + parsedData[20]);
         }
 
-        public static String readInto(String fileName) {
+        // This function is responsible for reading data from the file.
+        public static String[] parseInput(String fileName) throws FileNotFoundException {
+            Scanner input = new Scanner(new File(fileName));
+            String data = "";
 
-            String input;
+            // For each line, concatenate onto the data string.
+            while (input.hasNextLine()) {
+                // Add a comma at the end to aid the algorithm in splitting the string.
+                data += input.nextLine() + ",";
+            }
 
-            InputReader reader = new InputReader();
-            reader.fileName  = fileName;
-            reader.textRead = "";
-
-            reader.readInputFromFile();
-            input = reader.returnTextRead();
-
-            return input;
-
-        }
-
-        public static String[] parseInput(String input) {
-
-            String[] ret;
-
-            ParserInput p = new ParserInput();
-            p.inputNotParsed = input;
-
-            ret = p.returnParsedInput();
-
-            return ret;
-
-
+            // Split the data by the comma.
+            return data.split(",");
         }
     }
