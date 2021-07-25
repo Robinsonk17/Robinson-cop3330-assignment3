abcb064a3816b8bc9f85fc0704ca5c6c1dd539f6
Robinsonk17 <84465115+Robinsonk17@users.noreply.github.com>
2021-07-25 17:10:50 -0400

    Update solution41.java

--- src/main/java/oop/assignment3/ex41/base/solution41.java
+++ src/main/java/oop/assignment3/ex41/base/solution41.java
@@ -6,17 +6,21 @@ package oop.assignment3.ex41.base;
 import java.io.*;
 import java.util.*;
 
+// define the class solution41
 public class solution41
 {
+    // define inputNames() function to read the data from Scanner object to the ArrayList
     public static void inputNames(Scanner input, ArrayList<String> names)
     {
 
         while (input.hasNextLine())
         {
+            // add the next line to the list of names
             names.add(input.nextLine());
         }
     }
 
+    // define the function to print the names in the file
     public static void outputNames(ArrayList<String> names) throws IOException {
 
         FileWriter output = new FileWriter("exercise41_output.txt");
@@ -30,14 +34,17 @@ public class solution41
             output.write(i + "\n");
 
         }
+        // close the output FileWriter
         output.close();
 
     }
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        // create a scanner object to read from input file
         Scanner inputFile = new Scanner(new File("exercise41_input.txt"));
 
+        // declare an ArrayList for storing the names
         ArrayList<String> names = new ArrayList<String>();
 
         inputNames(inputFile, names);
@@ -49,6 +56,9 @@ public class solution41
         try {
             outputNames(names);
         }
+
+
+        // if any exception is raised exit output
 
         catch (Exception BYE) {
             System.out.println(BYE);
